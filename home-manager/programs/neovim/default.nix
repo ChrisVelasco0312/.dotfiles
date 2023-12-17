3f5eb7441e2aeb4ed1d4fae78f86005166270fe1
CrisVelasco0312 <cristianvelasco0312@gmail.com>
2023-12-17 16:52:46 -0500

    add neovim autocompletion and telescope

--- home-manager/programs/neovim/default.nix
+++ home-manager/programs/neovim/default.nix
@@ -24,21 +24,47 @@
       tree-sitter
       nodePackages.typescript
       nodePackages.typescript-language-server 
+      nodePackages.eslint
     ];
 
     extraLuaConfig = ''
       ${builtins.readFile ./nvim-lua/init.lua } 
       ${builtins.readFile ./nvim-lua/base.lua } 
+      ${builtins.readFile ./nvim-lua/maps.lua }
+      -- plugins config
       ${builtins.readFile ./nvim-lua/plugins/telescope.lua }
+      ${builtins.readFile ./nvim-lua/plugins/autopairs.lua }
+      ${builtins.readFile ./nvim-lua/plugins/bufferline.lua }
+      ${builtins.readFile ./nvim-lua/plugins/cmp.lua }
+      ${builtins.readFile ./nvim-lua/plugins/lsp-config.lua }
+      ${builtins.readFile ./nvim-lua/plugins/mason.lua }
+      ${builtins.readFile ./nvim-lua/plugins/onedark.lua }
     '';
 
     plugins = with pkgs.vimPlugins; [
       # you can use plugins from the pkgs
       vim-which-key
       nvim-treesitter.withAllGrammars
+      ## telescope
       telescope-nvim
       telescope-fzf-native-nvim
       telescope-file-browser-nvim
+      ## misc
+      nvim-autopairs
+      bufferline-nvim
+      ## autocompletion
+      nvim-cmp
+      cmp-nvim-lsp
+      lspkind-nvim
+      cmp_luasnip
+      luasnip
+      ##  lsp
+      nvim-lspconfig
+      mason-nvim
+      mason-lspconfig-nvim
+      neodev-nvim
+      ## theme
+      onedark-nvim
     ];
   };
 }
