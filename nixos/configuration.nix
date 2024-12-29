ba5d997de04b970b8370280f3f5950d7abe2c779
CrisVelasco0312 <cristianvelasco0312@gmail.com>
2024-12-28 20:38:30 -0500

    Add tuigreet login screen

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -14,10 +14,7 @@
   boot.loader.grub.enable = true;
   boot.loader.grub.device = "/dev/nvme0n1";
   boot.loader.grub.useOSProber = true;
-
-  networking.hostName = "nixos"; # Define your hostname.
-  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
-
+  networking.hostName = "nixos"; # Define your hostname. networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
   # Configure network proxy if necessary
   # networking.proxy.default = "http://user:password@proxy:port/";
   # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
@@ -47,7 +44,6 @@
     enable = true;
     enable32Bit = true;
   };
-
   hardware.nvidia = {
     modesetting.enable = true;
     powerManagement.enable = false;
@@ -57,29 +53,38 @@
     package = config.boot.kernelPackages.nvidiaPackages.stable;
   };
 
+  hardware.bluetooth.enable = true; # enables support for Bluetooth
+  hardware.bluetooth.powerOnBoot = true;
+  services.blueman.enable = true;
+  hardware.pulseaudio.enable = false;
+
+  services.greetd = {
+    enable = true;
+    vt = 3;
+    settings = {
+      default_session = {
+        user = "cavelasco";
+        command = "${pkgs.greetd.tuigreet}/bin/tuigreet --time -cmd Hyprland";
+      };
+    };
+  };
+
   services.xserver = {
     videoDrivers = ["nvidia"];
     enable = true;
     xkb.layout = "us, es";
     xkb.options = "erosign:e, compose:menu, grp:alt_space_toggle";
     xkb.variant = "";
+    wacom.enable = true;
   };
-
-  services.blueman.enable = true;
-
-  programs.hyprland.enable = true;
-
   services.libinput = {
     touchpad.naturalScrolling = true;
     enable = true;
     mouse.naturalScrolling = true;
   };
-
   # Enable CUPS to print documents.
   services.printing.enable = true;
-
-  # Enable sound with pipewire.
-  hardware.pulseaudio.enable = false;
+  # Enable sound with pipewire
   security.rtkit.enable = true;
   services.pipewire = {
     enable = true;
@@ -99,7 +104,6 @@
     ];
     shell = pkgs.zsh;
   };
-
   #docker
   virtualisation.docker.enable = true;
   virtualisation.docker.rootless = {
@@ -123,6 +127,7 @@
   ];
 
   programs.zsh.enable = true;
+  programs.hyprland.enable = true;
 
   fonts.packages = with pkgs; [
     fira-code
@@ -133,33 +138,6 @@
   nix.extraOptions = ''
     experimental-features = nix-command flakes
   '';
-  hardware.bluetooth.enable = true; # enables support for Bluetooth
-  hardware.bluetooth.powerOnBoot = true;
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
-
-  # List services that you want to enable:
-
-  # Enable the OpenSSH daemon.
-  # services.openssh.enable = true;
-
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
-
-  # This value determines the NixOS release from which the default
-  # settings for stateful data, like file locations and database versions
-  # on your system were taken. It‘s perfectly fine and recommended to leave
-  # this value at the release version of the first install of this system.
-  # Before changing this value read the documentation for this option
-  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-  system.stateVersion = "24.11"; # Did you read the comment?
+  system.stateVersion = "24.11"; 
 
 }
