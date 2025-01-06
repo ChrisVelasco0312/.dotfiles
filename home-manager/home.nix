752236af0b53208e73213b5f7721e01b99056cc2
ChrisVelasco0312 <cristianvelasco0312@gmail.com>
2025-01-06 17:36:34 -0500

    add spotify app

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -66,6 +66,7 @@
     (nerdfonts.override {
       fonts = ["JetBrainsMono" "Inconsolata"];
     })
+    # APPS
     obsidian # markdown note taking
     xournalpp # handwritten note taking
     okular # pdf viewer
@@ -74,6 +75,7 @@
     vlc # Cross-platform media player
     dolphin# file manager
     breeze-icons # icons
+    spotify # music stream
   ];
 
 	home.sessionVariables = {
