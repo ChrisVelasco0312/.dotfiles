9e7bb233347c30341b3754a2bb2fbb51fd30dd31
ChrisVelasco0312 <cristianvelasco0312@gmail.com>
2026-02-02 16:18:02 -0500

    back to 5 minutes per image

--- dots/hypr/background-cycle.py
+++ dots/hypr/background-cycle.py
@@ -35,7 +35,7 @@ LASTFM_USER = ENV["LASTFM_USER"]
 
 CACHE_FILE = os.path.expanduser("~/.config/tidal_session.json")
 CACHE_DIR = os.path.expanduser("~/.cache/album_covers")
-INTERVAL = 300  # 5 minutes per image
+INTERVAL = 600  # 5 minutes per image
 # ---------------------
 
 os.makedirs(CACHE_DIR, exist_ok=True)
